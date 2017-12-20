2679c3e9df44a3b4ea33974f0e6c5a9e5c53f961
azqasxws <121110350@qq.com>
2017-12-20 16:03:13 +0800

    zhuce

--- src/components/mine/mineComponent.js
+++ src/components/mine/mineComponent.js
@@ -1,7 +1,17 @@
 import React from 'react'
 import { Button, WhiteSpace, WingBlank } from 'antd-mobile';
+import { createForm } from 'rc-form';
+import './register/register.css'
 export default class MineComponent extends React.Component{
     render(){
-        return <Button >bbb</Button>
+        return (
+            <div>
+                <h1>注册</h1>
+                <label className="label"><span className="span">用户名</span><input  type="text" placeholder="请输入用户名" className="input"/></label><br/>
+                <label className="label"><span className="span">密码</span><input  type="password" placeholder="请输入密码" className="input"/></label><br/>
+                <label className="label"><span className="span">确认密码</span><input  type="password" placeholder="请确认密码" className="input"/></label><br/>
+                <input type="button" value="注册" />
+            </div>
+        )
     }
 }
