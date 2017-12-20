29cb819e0fe58f7d35846d7b86a11d412aa63390
duan <1543234759.com>
2017-12-20 11:43:04 +0800

    php文件夹

--- src/components/home/homeComponents.js
+++ src/components/home/homeComponents.js
@@ -15,34 +15,35 @@ export default class HomeComponent extends React.Component{
     }, 100);
      } 
     render(){
-        return <div>
-                    <Carousel
-                      autoplay={false}
-                      infinite={true}
-                      selectedIndex={1}
-                      beforeChange={(from, to) => console.log(`slide from ${from} to ${to}`)}
-                      afterChange={index => console.log('slide to', index)}
-                    >
-                      {this.state.data.map(ii => (
-                        <a
-                          key={ii}
-                          href="http://www.alipay.com"
-                          style={{ display: 'inline-block', width: '100%', height: this.state.imgHeight }}
-                        >
-                          <img
-                            src={`https://zos.alipayobjects.com/rmsportal/${ii}.png`}
-                            alt=""
-                            style={{ width: '100%', verticalAlign: 'top' }}
-                            onLoad={() => {
-                              // fire window resize event to change height
-                              window.dispatchEvent(new Event('resize'));
-                              this.setState({ imgHeight: 'auto' });
-                            }}
-                          />
-                        </a>
-                      ))}
-                    </Carousel>
-
-                </div>
+        return (
+          <div>
+            <Carousel
+              autoplay={false}
+              infinite={true}
+              selectedIndex={1}
+              beforeChange={(from, to) => console.log(`slide from ${from} to ${to}`)}
+              afterChange={index => console.log('slide to', index)}
+            >
+              {this.state.data.map(ii => (
+                <a
+                  key={ii}
+                  href="http://www.alipay.com"
+                  style={{ display: 'inline-block', width: '100%', height: this.state.imgHeight }}
+                >
+                  <img
+                    src={`https://zos.alipayobjects.com/rmsportal/${ii}.png`}
+                    alt=""
+                    style={{ width: '100%', verticalAlign: 'top' }}
+                    onLoad={() => {
+                      // fire window resize event to change height
+                      window.dispatchEvent(new Event('resize'));
+                      this.setState({ imgHeight: 'auto' });
+                    }}
+                  />
+                </a>
+              ))}
+            </Carousel>
+          </div>
+        )
     }
 }
