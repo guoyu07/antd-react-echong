83dead4eb147d59187332c9f793207aaeb0b8652
Dragon-Xiang <506840830@qq.com>
2017-12-25 10:56:15 +0800

    差不多了

--- php/homeSearch.php
+++ php/homeSearch.php
@@ -4,8 +4,8 @@
     include "DBHelper.php";
 
     $searchText=isset($_GET['searchText'])?$_GET['searchText']:'';
+    $sql = "select * from dogclothes";
     if($searchText){
-        $sql = "select * from dogclothes";
         $sql .=" where concat(gooddetail,goodaddress,goodclassify,gooddetail) like '%$searchText%'";
     }
     $result = query($sql);
